009db6595a53cce438569b8287b259e656b2489b
IlaySkutelsky <40234996+IlaySkutelsky@users.noreply.github.com>
2021-04-05 15:33:43 +0300

    Added my events

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const start = document.timeline.currentTime
 
+let myEvents = [
+  {seconds: 5, vibrationPattern: [200,200,200], text: "hey hey ho ho hopa hopa"},
+  {seconds: 10, vibrationPattern: 200, text: "2"},
+  {seconds: 15, vibrationPattern: 400, text: "3"},
+  {seconds: 25, vibrationPattern: [200,200,200], text: "no no 4"}
+]
+
+let currentEventIndex = 0
+
 function frame(time) {
   const elapsed = time - start
   const seconds = Math.round(elapsed / 1000)
@@ -14,8 +23,12 @@ function frame(time) {
 function updateUI(seconds) {
   let counterElm = document.querySelector(".counter")
   if (counterElm) counterElm.innerText = seconds
-  if (seconds%10 != 0) return
-  window.navigator.vibrate(200)
+  if (seconds === myEvents[currentEventIndex].seconds) {
+    window.navigator.vibrate(myEvents[currentEventIndex].vibrationPattern)
+    let textElm = document.querySelector(".text")
+    if (textElm) textElm.innerText = myEvents[currentEventIndex].text
+    currentEventIndex++
+  }
   // let notification = new Notification("Secoonds: " + seconds, {
   //   vibrate: 200
   // });
