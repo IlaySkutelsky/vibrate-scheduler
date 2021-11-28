fa1895133c142b411fa993f155506d892332b5a4
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2021-11-28 17:36:27 +0200

    MusraraMix21 times V2

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 let start
 
 let myEvents = [
-  {seconds: "00:05", vibrationPattern: 200, text: "test0"},
+  {seconds: "00:05", vibrationPattern: 200, text: "test1"},
   {seconds: "01:48", vibrationPattern: [400,200,400], text: "Proud to present, go to door 1"},
   {seconds: "02:01", vibrationPattern: [400,200,400], text: "get ready to exit 1"},
   {seconds: "02:04", vibrationPattern: 600, text: "exit 1"},
-  {seconds: "03:04", vibrationPattern: 600, text: "enter 1"},
-  {seconds: "03:32", vibrationPattern: [400,200,400], text: "walk to door 2"},
-  {seconds: "03:40", vibrationPattern: [400,200,400], text: "exit 2"},
-  {seconds: "04:13", vibrationPattern: 600, text: "enter 2"},
-  {seconds: "05:19", vibrationPattern: [400,200,400], text: "walk to door 3"},
-  {seconds: "05:24", vibrationPattern: [600], text: "exit 3"},
+  {seconds: "03:05", vibrationPattern: 600, text: "enter 1"},
+  {seconds: "03:46", vibrationPattern: [400,200,400], text: "walk to door 2"},
+  {seconds: "03:54", vibrationPattern: [400,200,400], text: "exit 2"},
+  {seconds: "04:27", vibrationPattern: 600, text: "enter 2"},
+  {seconds: "05:20", vibrationPattern: [400,200,400], text: "walk to door 3"},
+  {seconds: "05:26", vibrationPattern: [600], text: "exit 3"},
   {seconds: "07:10", vibrationPattern: 600, text: "enter 3"}
 ]
-
+// 3:16 ghosts video end
+// 5:26 exit 3
+// 7: 10 enter 3
 let currentEventIndex = 0
 
 function frame(time) {
